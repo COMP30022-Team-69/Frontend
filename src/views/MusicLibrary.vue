fb05757b2db49fc8bc5e0a1f411bc10ff8d57593
OliviaDu1011 <yipingdi@student.unimelb.edu.au>
2023-08-23 14:50:37 +1000

    initial commit

---
+++ src/views/MusicLibrary.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>音乐库</h1>
+    <div v-for="music in musics" :key="music.id">
+      <MusicItem
+        :music="music"
+        @dragstart="onDragStart($event, music)"
+        @remove-from-playlist="handleRemoveFromPlaylist"
+        @toggle-favorite="handleToggleFavorite"
+      />
+    </div>
+    <!-- 个人歌单分类区 -->
+    <div>
+      <!-- 个人歌单分类区的代码 -->
+    </div>
+  </div>
+</template>
+
+<script>
+import MusicItem from '@/components/MusicItem.vue';
+
+export default {
+  components: {
+    MusicItem,
+  },
+  data() {
+    return {
+      musics: [/* 音乐数据 */],
+    };
+  },
+  methods: {
+    onDragStart(event, music) {
+      // 已有的拖放逻辑
+    },
+    handleRemoveFromPlaylist(id) {
+      // 实现从歌单中删除歌曲的逻辑
+    },
+    handleToggleFavorite(id) {
+      // 实现标记或取消标记喜爱歌曲的逻辑
+    },
+  },
+};
+</script>
