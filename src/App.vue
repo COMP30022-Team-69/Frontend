995518c4189373a9d3211472504e58cf8fb28d38
EASONOSAE <127077771+EASONOSAE@users.noreply.github.com>
2023-09-06 16:23:56 +1000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <template>
-  <router-view />
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title>MUSIC LIBRARY</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-avatar>
+        <img src="url_to_user_avatar" alt="User" />
+      </v-avatar>
+      <v-btn @click="logout" >Log Out</v-btn>
+    </v-app-bar>
+    <v->
+    </v->>
+    <router-view class="mt-10"/>
+  </v-app>
+
 </template>
 
 <script setup>
