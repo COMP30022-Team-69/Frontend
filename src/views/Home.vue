d457504c33949c306be049817852b4788e008e48
EASONOSAE <127077771+EASONOSAE@users.noreply.github.com>
2023-09-06 16:23:23 +1000

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,43 @@
 <template>
-  <HelloWorld />
+    <v-container class="mt-4">
+      <v-row>
+        <v-col cols="12" md="3">
+          <v-list>
+            <v-list-item v-for="(playlist, index) in playlists" :key="index" @click="selectPlaylist(index)">
+              <v-list-item-title>{{ playlist }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-col>
+
+        <v-col cols="12" md="8">
+          <!-- Content of the selected playlist -->
+          <v-card v-if="selectedPlaylist !== null">
+            <v-card-title>{{ playlists[selectedPlaylist] }}</v-card-title>
+            <!-- Display songs and other content related to the selected playlist -->
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
-<script setup>
-  import HelloWorld from '@/components/HelloWorld.vue'
+<script>
+export default {
+  data() {
+    return {
+      playlists: ['Favourite', 'Background', 'Relax', 'Sleep'],
+      selectedPlaylist: null,
+    };
+  },
+  methods: {
+    selectPlaylist(index) {
+      this.selectedPlaylist = index;
+    },
+    logout() {
+      // log out to the login page
+    },
+  },
+};
 </script>
+
+<style>
+</style>
