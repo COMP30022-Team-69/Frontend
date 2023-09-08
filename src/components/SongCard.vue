489bb838719de2bd516b24a913ad22cedeebc4d4
HenrikHao <73416544+HenrikHao@users.noreply.github.com>
2023-09-08 23:27:54 +1000

    add SongCard

---
+++ src/components/SongCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card class="mb-2">
+    <v-card-title>{{ song.title }}</v-card-title>
+    <v-card-subtitle>{{ song.artist }}</v-card-subtitle>
+    <!-- You can add more details or actions for each song here -->
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    song: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Add any specific styles for the song card here */
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,16 +3,14 @@
     <v-row>
       <v-col cols="12" md="3">
         <!-- Sidebar component -->
-        <Sidebar :playlists="playlists" @select="selectPlaylist" />
+        <Sidebar :playlists="Object.keys(playlists)" @select="selectPlaylist" />
       </v-col>
 
-      <v-col cols="12" md="8">
-        <!-- Content of the selected playlist -->
-        <v-card v-if="selectedPlaylist !== null">
-          <v-card-title>{{ playlists[selectedPlaylist] }}</v-card-title>
-          <!-- Display songs and other content related to the selected playlist -->
-          <!-- ... -->
-        </v-card>
+      <v-col cols="12" md="9">
+        <div v-if="selectedPlaylist !== null">
+          <h2>{{ selectedPlaylist }}</h2>
+          <SongCard v-for="song in playlists[selectedPlaylist].songs" :key="song.id" :song="song" />
+        </div>
       </v-col>
     </v-row>
   </v-container>
@@ -20,27 +18,46 @@
 
 <script>
 import Sidebar from "@/components/Sidebar.vue";
+import SongCard from "@/components/SongCard.vue";
 
 export default {
   components: {
-    Sidebar
+    Sidebar,
+    SongCard
   },
   data() {
     return {
-      playlists: ['Favourite', 'Background', 'Relax', 'Sleep'],
+      playlists: {
+        'Favourite': {
+          name: 'Favourite',
+          songs: [
+            { id: 1, title: 'Song A', artist: 'Artist A' },
+            { id: 2, title: 'Song B', artist: 'Artist B' },
+            // ... other songs
+          ]
+        },
+        'Background': {
+          name: 'Background',
+          songs: [
+            { id: 3, title: 'Song C', artist: 'Artist C' },
+            // ... other songs
+          ]
+        },
+        // ... other categories like 'Relax', 'Sleep', etc.
+      },
       selectedPlaylist: null
     };
   },
   methods: {
     selectPlaylist(index) {
-      this.selectedPlaylist = index;
+      const playlistNames = Object.keys(this.playlists);
+      this.selectedPlaylist = playlistNames[index];
     },
-    // ... other methods, like logout
+    // ... other methods, like logout (if you have them)
   }
 };
 </script>
 
 <style scoped>
 /* Add any specific styles for the Home view here */
-
 </style>
