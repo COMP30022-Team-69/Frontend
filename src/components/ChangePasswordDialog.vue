e43f9b23da63c86ff2bcdcd840c0b93b78f7a44a
wisdommen <41609381+wisdommen@users.noreply.github.com>
2023-09-30 22:58:44 +1000

    Update add github workflow

--- src/components/ChangePasswordDialog.vue
+++ src/components/ChangePasswordDialog.vue
@@ -67,7 +67,13 @@ export default {
         user.login(store.state.user.data.username, this.currentPassword, (res) => {
           user.updateUserPassword(this.newPassword, (res) => {
             SnackBar.Launch("Password changed successfully!");
+            user.getUserById(store.state.user.data._id, (res) => {
+              store.state.user.data = res.data
+            })
             this.dialog = false;
+            this.currentPassword = ""
+            this.newPassword = ""
+            this.confirmPassword = ""
           });
         }, (err) => {
           this.wrongPassword = true
